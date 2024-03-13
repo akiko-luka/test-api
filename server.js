aadd7a0f09eeefeffc4459ee5ca7b8a24e729de5
akiko-luka <ak.luka051@gmail.com>
2024-03-13 14:56:48 +0100

    add weather endpoint

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express, { response } from "express";
 
-console.log();
 const app = express();
 const PORT = process.env.PORT
 const environment = process.env.NODE_ENV;
+const API_KEY = process.env.API_KEY
 
 app.get("/", (req, res) => {
   res.send("welcome");
@@ -19,6 +19,12 @@ app.get("/greeting", (req, res) => {
   const greetingIndex = Math.floor(Math.random() * greetings.length);
   res.json({ value: greetings[greetingIndex] });
 });
+
+app.get("/weather/:location", async (req, res) => {
+  const response = await fetch(`https://api.tomorrow.io/v4/weather/forecast?location=${req.params.location}&apikey=${API_KEY}`)
+  const data = await response.json()
+  res.send(data)
+})
 
 app.listen(PORT, () => {
   if(environment === "development") {
