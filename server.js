f37640a64c8bcc3be8f93949c39f694d00400e01
akiko-luka <ak.luka051@gmail.com>
2024-03-13 11:36:48 +0100

    fix

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ app.get("/", (req, res) => {
   res.send("welcome")
 })
 
-app.get("r/andom-number", (req,res) {
+app.get("/random-number", (req,res) => {
   const number = Math.floor(Math.random() * 100)
   res.json({value: number})
 })
 
-app.get("/greeting", (req,res) {
+app.get("/greeting", (req,res) => {
   const greetings = ["hey", "hi", "hola", "ciao"]
   const greetingIndex = Math.floor(Math.random() * greetings.length)
   res.json({value: greetings[greetingIndex]})
